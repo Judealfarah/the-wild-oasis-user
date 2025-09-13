5c65f372c1df4440970e39a83c92c1f40b8ff466
Jude AlFarah <>
2025-09-13 11:35:49 +0200

    initial

---
+++ app/_components/CabinList.js
@@ -0,0 +1,35 @@
+import CabinCard from "./CabinCard";
+import { getCabins } from "../_lib/data-service";
+
+async function CabinList({ filter }) {
+  const cabins = await getCabins();
+
+  if (!cabins.length) return null;
+
+  let displayCabins;
+
+  if (filter === "all") {
+    displayCabins = cabins;
+  }
+  if (filter === "small") {
+    displayCabins = cabins.filter((cabin) => cabin.max_capacity <= 3);
+  }
+  if (filter === "medium") {
+    displayCabins = cabins.filter(
+      (cabin) => cabin.max_capacity >= 4 && cabin.max_capacity <= 7
+    );
+  }
+  if (filter === "large") {
+    displayCabins = cabins.filter((cabin) => cabin.max_capacity >= 8);
+  }
+
+  return (
+    <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
+      {displayCabins.map((cabin) => (
+        <CabinCard cabin={cabin} key={cabin.id} />
+      ))}
+    </div>
+  );
+}
+
+export default CabinList;
